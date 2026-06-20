95cb82a55b51bb6da515557c6a81ed35c2f35cfb
sebastianhieatt <145358091+sebastianhieatt@users.noreply.github.com>
2026-06-20 14:27:44 -0700

    Fixing Navigation to StoreListView
    Content view didn't link correctly to the StoreListView, since the buttons weren't wrapped inside of a NavigationView

--- StoopingProject/ContentView.swift
+++ StoopingProject/ContentView.swift
@@ -1,35 +1,35 @@
 import SwiftUI
 
 struct ContentView: View {
-
     var body: some View {
-
         TabView {
+                    
+                    HomeView()
+                        .tabItem {
+                            Image(systemName: "house.fill")
+                            Text("Home")
+                        }
+                    NavigationView {
+                        StoreListView()
+                    }
+                    .tabItem {
+                        Image(systemName: "bag.fill")
+                        Text("Shop")
+                    }
 
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                }
+                    NavigationView {
+                            CheckoutPageView()
+                        }
+                        .tabItem {
+                            Image(systemName: "cart.fill")
+                            Text("Checkout")
+                        }
 
-            StoreListView()
-                .tabItem {
-                    Image(systemName: "bag.fill")
-                    Text("Shop")
+                    DonationsView()
+                        .tabItem {
+                            Image(systemName: "heart.fill")
+                            Text("Donate")
+                        }
                 }
-
-            CheckoutPageView()
-                .tabItem {
-                    Image(systemName: "cart.fill")
-                    Text("Checkout")
-                }
-
-            DonationsView()
-                .tabItem {
-                    Image(systemName: "heart.fill")
-                    Text("Donate")
-                }
-        }
-        .accentColor(AppTheme.primaryGreen)
     }
 }
